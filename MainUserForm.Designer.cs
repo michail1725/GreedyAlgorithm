387cd1e248aa65cccfb0054bb9890ba5ac70cc6b
Michael Sharonov <47724308+michail1725@users.noreply.github.com>
2022-08-25 23:52:31 +0300

    Update content

--- MainUserForm.Designer.cs
+++ MainUserForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HungryAlgorithm
 {
-    partial class Form1
+    partial class MainUserForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -30,10 +30,11 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Simulate = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.StipendValue = new System.Windows.Forms.ComboBox();
+            this.Maximize = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -45,7 +46,7 @@
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 87);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -54,7 +55,7 @@
             // 
             // Simulate
             // 
-            this.Simulate.Location = new System.Drawing.Point(12, 36);
+            this.Simulate.Location = new System.Drawing.Point(12, 58);
             this.Simulate.Name = "Simulate";
             this.Simulate.Size = new System.Drawing.Size(306, 23);
             this.Simulate.TabIndex = 2;
@@ -62,28 +63,28 @@
             this.Simulate.UseVisualStyleBackColor = true;
             this.Simulate.Click += new System.EventHandler(this.Simulate_Click);
             // 
-            // comboBox1
+            // StipendValue
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.StipendValue.FormattingEnabled = true;
+            this.StipendValue.Items.AddRange(new object[] {
             "2600",
             "3400",
             "4500"});
-            this.comboBox1.Location = new System.Drawing.Point(28, 9);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(197, 21);
-            this.comboBox1.TabIndex = 3;
-            this.comboBox1.Text = "Выберите размер стипендии";
+            this.StipendValue.Location = new System.Drawing.Point(12, 31);
+            this.StipendValue.Name = "StipendValue";
+            this.StipendValue.Size = new System.Drawing.Size(197, 21);
+            this.StipendValue.TabIndex = 3;
+            this.StipendValue.Text = "Выберите размер стипендии";
             // 
-            // button2
+            // Maximize
             // 
-            this.button2.Location = new System.Drawing.Point(381, 36);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(247, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Максимизировать выбор";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.Maximize.Location = new System.Drawing.Point(355, 58);
+            this.Maximize.Name = "Maximize";
+            this.Maximize.Size = new System.Drawing.Size(297, 23);
+            this.Maximize.TabIndex = 4;
+            this.Maximize.Text = "Максимизировать выбор";
+            this.Maximize.UseVisualStyleBackColor = true;
+            this.Maximize.Click += new System.EventHandler(this.Maximize_Click);
             // 
             // dataGridView2
             // 
@@ -91,36 +92,48 @@
             this.dataGridView2.AllowUserToResizeRows = false;
             this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(355, 104);
+            this.dataGridView2.Location = new System.Drawing.Point(355, 109);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
             this.dataGridView2.RowHeadersVisible = false;
             this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.dataGridView2.Size = new System.Drawing.Size(297, 306);
+            this.dataGridView2.Size = new System.Drawing.Size(297, 323);
             this.dataGridView2.TabIndex = 5;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(432, 65);
+            this.label1.Location = new System.Drawing.Point(352, 87);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.Size = new System.Drawing.Size(300, 19);
             this.label1.TabIndex = 6;
-            this.label1.Text = "На 0 руб. мы смогли купить: ";
+            this.label1.Text = "На 0 руб. мы могли бы купить: ";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Стипендия:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(680, 422);
+            this.ClientSize = new System.Drawing.Size(680, 453);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Maximize);
+            this.Controls.Add(this.StipendValue);
             this.Controls.Add(this.Simulate);
             this.Controls.Add(this.dataGridView1);
+            this.MaximumSize = new System.Drawing.Size(696, 492);
+            this.MinimumSize = new System.Drawing.Size(696, 492);
             this.Name = "Form1";
-            this.Text = "I am greedy";
+            this.Text = "I\'m greedy";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
@@ -132,10 +145,11 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button Simulate;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox StipendValue;
+        private System.Windows.Forms.Button Maximize;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
---
+++ MainUserForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace HungryAlgorithm
+{
+    public partial class MainUserForm : Form
+    {
+        DataTable dt = new DataTable();
+        public MainUserForm()
+        {
+            InitializeComponent();
+            FillProductTabel();
+        }
+
+        private void FillProductTabel() { //заполнение списка потребительской корзины
+            dt.Columns.Add("Название");
+            dt.Columns.Add("Цена(руб.)", typeof(int));
+            dt.Columns.Add("Оценка");
+            dt.Columns.Add("Отношение");
+            dt.Rows.Add("Хлеб", 50, "", "");
+            dt.Rows.Add("Проездной", 1200, "", "");
+            dt.Rows.Add("Интернет", 550, "", "");
+            dt.Rows.Add("Колбаса", 300, "", "");
+            dt.Rows.Add("Сыр", 200, "", "");
+            dt.Rows.Add("Масло", 200, "", "");
+            dt.Rows.Add("Молоко", 60, "", "");
+            dt.Rows.Add("Моб. связь", 500, "", "");
+            dt.Rows.Add("Туалетная бумага", 200, "", "");
+            dt.Rows.Add("Канцелярия", 240, "", "");
+            dt.Rows.Add("Чай", 300, "", "");
+            dt.Rows.Add("Кофе", 250, "", "");
+            dt.Rows.Add("Общежитие", 1100, "", "");
+            dt.Rows.Add("Шампунь", 180, "", "");
+            dt.Rows.Add("Зубная паста", 100, "", "");
+            dt.Rows.Add("Пряники", 60, "", "");
+            dt.Rows.Add("Сок", 90, "", "");
+            dt.Rows.Add("Лимонад", 70, "", "");
+            dt.Rows.Add("Кроссовки", 3500, "", "");
+            dt.Rows.Add("Лекарства", 800, "", "");
+            dt.Rows.Add("Сушки", 40, "", "");
+            dt.Rows.Add("Чипсы", 160, "", "");
+            dt.Rows.Add("Мясо", 340, "", "");
+            dt.Rows.Add("Греча", 100, "", "");
+            dt.Rows.Add("Спички", 5, "", "");
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Simulate_Click(object sender, EventArgs e)//симуляция приоритетов
+        {
+            Random rnd = new Random();
+           
+            for (int i = 0; i < dt.Rows.Count; i++) {
+                dt.Rows[i][2] = rnd.Next(10, 100);
+                dt.Rows[i][3] = Math.Round(Convert.ToDouble(dt.Rows[i][2]) / Convert.ToDouble(dt.Rows[i][1]),3);
+            }
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Maximize_Click(object sender, EventArgs e)// максимизация выбора продуктов на основании расчитанных коэффициентов 
+        {
+            if (StipendValue.SelectedIndex != -1)
+            {
+                label1.Visible = true;
+                DataView dv = dt.DefaultView;
+                dv.Sort = "Отношение";
+                dt = dv.ToTable();
+                int bank = Convert.ToInt32(StipendValue.Text);
+                int count = dt.Rows.Count;
+                DataTable dt1 = new DataTable();
+                dt1.Columns.Add("Название");
+                dt1.Columns.Add("Цена(руб.)", typeof(int));
+                dt1.Columns.Add("Оценка");
+                dt1.Columns.Add("Отношение");
+                while (bank != 0 && count != 0)
+                {
+                    if ((bank - Convert.ToInt32(dt.Rows[count - 1][1])) >= 0)
+                    {
+                        bank -= Convert.ToInt32(dt.Rows[count - 1][1]);
+                        dt1.Rows.Add(dt.Rows[count - 1][0], dt.Rows[count - 1][1], dt.Rows[count - 1][2], dt.Rows[count - 1][3]);
+                    }
+                    count -= 1;
+                }
+                dataGridView2.DataSource = dt1;
+                label1.Text = "На " + StipendValue.Text + " руб.(ост. " + bank + " руб.)" +  " мы можем купить " + dt1.Rows.Count + " позиций:";
+                dv = dt1.DefaultView;
+                dv.Sort = "Отношение DESC";
+                dt1 = dv.ToTable();
+            }
+            else {
+                MessageBox.Show("Значение стипендии не выбрано!");
+            }
+            
+        }
+    }
+}
